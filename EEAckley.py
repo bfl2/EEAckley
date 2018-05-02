dedddbabb52f3338e5a3f58712054bc20801d1c6
bfl2 <brunofilizolaleal@gmail.com>
2018-05-01 23:05:41 -0300

    Adicionada geracao inicial da populacao

--- EEAckley.py
+++ EEAckley.py
@@ -1,7 +1,8 @@
-import crossOver
-import mutation
+import crossOver as cross
+import mutation as mut
 import random
 import numpy as np
+from operator import itemgetter
 
 
 def generateIndivI():
@@ -13,11 +14,28 @@ def generateIndivI():
     return indiv
 def generateIndiv():
     n = 30
-    indiv = []
-    while (len(indiv) < n):
-        indiv.append(round(random.uniform(-15, 15), 5))
-
+    chromossome= []
+    while (len( chromossome) < n):
+        chromossome.append(round(random.uniform(-15, 15), 5))
+    fit = fitness(chromossome)
+    indiv = [chromossome,fit]
     return indiv
+
+def generatePop(size):
+    pop = []
+    while (len(pop)< size):
+        pop.append(generateIndiv())
+    pop = sorted(pop, key=itemgetter(1))
+    return pop
+
+def generateChildren(parents,childrenCount):
+    children = []
+    child = []
+    while (len(children)):
+        #child = crossover()
+        #child = mutation()
+        children.append(child)
+
 
 def fitness(chromossome):
     c1=20
@@ -33,7 +51,18 @@ def fitness(chromossome):
     fit = -c1*np.exp(-c2*np.sqrt(sum1)) - np.exp(sum2) + c1 + 1
     return round(fit,5)
 
+
+
 def EEAckley():
+
+    childrenCount = 10
+    parentCount = 70
+    condSaida = False
+    parents = generatePop(parentCount) # Populacao inicial
+
+    while(condSaida == False):
+
+        childrens = generateChildren(parents,childrenCount)
 
 
     return
