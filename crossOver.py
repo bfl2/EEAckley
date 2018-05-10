501485c43dc97d21cbb57e5f8f91b78e26e1e6a9
Decezaris Augusto <damp@cin.ufpe.br>
2018-05-10 20:46:25 -0300

    Crossover adaptado para diferentes sigmas (lista ou float)

--- crossOver.py
+++ crossOver.py
@@ -1,5 +1,6 @@
 ﻿from random import randint
-from EEAckley import fitness
+froom EEAckley import fitness
+
 
 #   Input Format ([Chromosome, Fitness, sigma])
 def recombination_2fixed_parents(parent1, parent2):
@@ -12,13 +13,24 @@ def recombination_2fixed_parents(parent1, parent2):
     sigma_1 = parent1[2]
     sigma_2 = parent2[2]
 
+    is_float = False
+
     zip_parents = zip(parent_1, parent_2)
-    zip_sigmas = zip(sigma_1, sigma_2)
+
+    if type(sigma_1) is float:
+        sigma_1 = [sigma_1]
+        sigma_2 = [sigma_2]
+        is_float = True
+
+    zip_sigmas = zip(list(sigma_1), list(sigma_2))
 
     child = [(x[0] + x[1])/2 for x in zip_parents]
     sigma_child = [(s[0] + s[1])/2 for s in zip_sigmas]
 
     fitness_child = fitness(child)
+
+    if is_float:
+        sigma_child = sigma_child[0]
 
     return [child, fitness_child, sigma_child]
 
@@ -32,13 +44,24 @@ def recombination_2fixed_random(parent1, parent2):
     sigma_1 = parent1[2]
     sigma_2 = parent2[2]
 
+    is_float = False
+
     zip_parents = zip(parent_1, parent_2)
+
+    if type(sigma_1) is float:
+        sigma_1 = [sigma_1]
+        sigma_2 = [sigma_2]
+        is_float = True
+
     zip_sigmas = zip(sigma_1, sigma_2)
 
     child = [x[randint(0, 1)] for x in zip_parents]
     sigma_child = [(s[0] + s[1]) / 2 for s in zip_sigmas]
 
     fitness_child = fitness(child)
+
+    if is_float:
+        sigma_child = sigma_child[0]
 
     return [child, fitness_child, sigma_child]
 
@@ -52,18 +75,27 @@ def recombination_all_parents(all_parents):
     tuple_sigmas = []
     size = len(all_parents)
     num_gene = len(all_parents[0][0])
+    is_float = False
 
     for i in range(num_gene):
         p1 = all_parents[randint(0, size - 1)]
         p2 = all_parents[randint(0, size - 1)]
 
         tuple_parents.append((p1[0][i], p2[0][i]))
-        tuple_sigmas.append((p1[2][i], p2[2][i]))
+
+        if type(p1[2]) is float:
+            tuple_sigmas.append((p1[2], p2[2]))
+            is_float = True
+        else:
+            tuple_sigmas.append((p1[2][i], p2[2][i]))
 
     child = [(x[0] + x[1])/2 for x in tuple_parents]
     sigma_child = [(s[0] + s[1])/2 for s in tuple_sigmas]
 
     fitness_child = fitness(child)
+
+    if is_float:
+        sigma_child = sigma_child[randint(0, num_gene-1)]
 
     return [child, fitness_child, sigma_child]
 
@@ -77,18 +109,27 @@ def recombination_all_random(all_parents):
     tuple_sigmas = []
     size = len(all_parents)
     num_gene = len(all_parents[0][0])
+    is_float = False
 
     for i in range(num_gene):
         p1 = all_parents[randint(0, size - 1)]
         p2 = all_parents[randint(0, size - 1)]
 
         tuple_parents.append((p1[0][i], p2[0][i]))
-        tuple_sigmas.append((p1[2][i], p2[2][i]))
+
+        if type(p1) is float:
+            tuple_sigmas.append((p1[2], p2[2]))
+            is_float = True
+        else:
+            tuple_sigmas.append((p1[2][i], p2[2][i]))
 
     child = [x[randint(0, 1)] for x in tuple_parents]
     sigma_child = [s[randint(0, 1)] for s in tuple_sigmas]
 
     fitness_child = fitness(child)
+
+    if is_float:
+        sigma_child = sigma_child[randint(0, num_gene-1)]
 
     return [child, fitness_child, sigma_child]
 
@@ -96,8 +137,8 @@ def recombination_all_random(all_parents):
 #   Basic Test
 if __name__ == '__main__':
 
-    p1 = [list(range(30)), 12.3, list(range(30))]
-    p2 = [list(range(30)), 52.3, list(range(30))]
+    p1 = [list(range(30)), 12.3, 5.0]
+    p2 = [list(range(30)), 52.3, 2.0]
 
     pop = [p1, p2]
     print(recombination_2fixed_parents(p1, p2))
