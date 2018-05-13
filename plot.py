14fb804cb926a9f37d884f51303c6542844c8205
Decezaris Augusto <damp@cin.ufpe.br>
2018-05-13 17:59:14 -0300

    Alterações no acesso do dicionário

--- plot.py
+++ plot.py
@@ -1,9 +1,10 @@
-import EEAckley as ack
+import EEAckley2 as ack
 import numpy as np
-import matplotlib
 from matplotlib import pyplot as plt
 
+
 def dummyDataset():
+
     avgFit = np.linspace(16,0,100)
     minFit = (16*np.exp(-avgFit))[::-1]
     indivFitness = [minFit,minFit,minFit]
@@ -11,17 +12,19 @@ def dummyDataset():
 
     return dataset
 
+
 def plotGraf():
-    dataset =  ack.EEAckley()
+
+    dataset =  ack.EEAckley2()
     """
     A implementar: 
     Calcular a media e dp de 30 iteracoes e escolher uma iteracao com valor proximo
     """
 
-    print("AvgFit:{} | MinFit:{}".format(dataset["avgFit"],dataset["minFit"]))
+    print("AvgFit:{} | MinFit:{}".format(dataset["avgFitList"], dataset["minFit"]))
     #dataset = dummyDataset()
-    fig,axes = plt.subplots(2,2)
-    x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
+    fig, axes = plt.subplots(2, 2)
+    x = np.linspace(0, dataset["generation"],dataset["generation"])
     y = dataset["avgFitList"]
     print(len(x),len(y))
     axes[0,0].plot(x,y)
@@ -39,6 +42,7 @@ def plotGraf():
 
     plt.show()
     return
+
 
 if __name__ == "__main__":
     plotGraf()
