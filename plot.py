78c0ab2f36d6a32dc33bf7f3cb93a8b8dd8033ee
bfl2 <brunofilizolaleal@gmail.com>
2018-05-13 20:20:52 -0300

    Plot das duas versoes implementados

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
-import EEAckley2 as ack
+import EEAckley2 as ack2
+import EEAckley as ack
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -15,16 +16,12 @@ def dummyDataset():
 
 def plotGraf():
 
-    dataset =  ack.EEAckley2()
-    """
-    A implementar: 
-    Calcular a media e dp de 30 iteracoes e escolher uma iteracao com valor proximo
-    """
+    dataset =  ack.EEAckley()
 
     print("AvgFit:{} | MinFit:{}".format(dataset["avgFitList"], dataset["minFit"]))
     #dataset = dummyDataset()
     fig, axes = plt.subplots(2, 2)
-    x = np.linspace(0, dataset["generation"],dataset["generation"])
+    x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
     y = dataset["avgFitList"]
     print(len(x),len(y))
     axes[0,0].plot(x,y)
@@ -32,13 +29,37 @@ def plotGraf():
     axes[0, 0].set_ylabel("Fitness Medio")
     axes[0, 0].set_xlabel("Geracao")
 
-    #x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
+    dataset = ack2.EEAckley2()
+    x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
     y = dataset["minFitList"]
     print(len(x), len(y))
     axes[0, 1].plot(x, y)
-    axes[0, 1].set_title("Fitness Minimo da versao 1")
+    axes[0, 1].set_title("Fitness Minimo da versao 2")
     axes[0, 1].set_ylabel("Fitness Minimo")
     axes[0, 1].set_xlabel("Geracao")
+
+
+    print("AvgFit:{} | MinFit:{}".format(dataset["avgFitList"], dataset["minFit"]))
+    #dataset = dummyDataset()
+    x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
+    y = dataset["avgFitList"]
+    print(len(x),len(y))
+    axes[1,0].plot(x,y)
+    axes[1, 0].set_title("Media de fitness da versao 2")
+    axes[1, 0].set_ylabel("Fitness Medio")
+    axes[1, 0].set_xlabel("Geracao")
+
+    #x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
+    y = dataset["minFitList"]
+    print(len(x), len(y))
+    axes[1, 1].plot(x, y)
+    axes[1, 1].set_title("Fitness Minimo da versao 1")
+    axes[1, 1].set_ylabel("Fitness Minimo")
+    axes[1, 1].set_xlabel("Geracao")
+
+
+
+
 
     plt.show()
     return
