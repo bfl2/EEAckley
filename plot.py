84d0666f00d6cfe552d97a23845ac95266de675f
bfl2 <brunofilizolaleal@gmail.com>
2018-05-14 00:33:01 -0300

    Adicionada informacoes no plot

--- plot.py
+++ plot.py
@@ -23,13 +23,15 @@ def plotGraf():
     fig, axes = plt.subplots(2, 2)
     x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
     y = dataset["avgFitList"]
+    stringData2 = "Fitness Minimo: "+ str(round(dataset["minFit"],5))
+    stringData1 = "Fitness Medio: " + str(round(dataset["avgFit"],5))
     print(len(x),len(y))
     axes[0,0].plot(x,y)
     axes[0, 0].set_title("Media de fitness da versao 1")
     axes[0, 0].set_ylabel("Fitness Medio")
     axes[0, 0].set_xlabel("Geracao")
+    axes[0, 0].annotate(stringData1,(15,12))
 
-    dataset = ack2.EEAckley2()
     x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
     y = dataset["minFitList"]
     print(len(x), len(y))
@@ -37,10 +39,13 @@ def plotGraf():
     axes[0, 1].set_title("Fitness Minimo da versao 1")
     axes[0, 1].set_ylabel("Fitness Minimo")
     axes[0, 1].set_xlabel("Geracao")
+    axes[0, 1].annotate(stringData2, (15, 12))
 
+    dataset = ack2.EEAckley2()
 
     print("AvgFit:{} | MinFit:{}".format(dataset["avgFitList"], dataset["minFit"]))
-    #dataset = dummyDataset()
+    stringData2 = "Fitness Minimo: " + str(round(dataset["minFit"], 5))
+    stringData1 = "Fitness Medio: " + str(round(dataset["avgFit"], 5))
     x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
     y = dataset["avgFitList"]
     print(len(x),len(y))
@@ -48,6 +53,7 @@ def plotGraf():
     axes[1, 0].set_title("Media de fitness da versao 2")
     axes[1, 0].set_ylabel("Fitness Medio")
     axes[1, 0].set_xlabel("Geracao")
+    axes[1, 0].annotate(stringData1, (15, 12))
 
     #x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
     y = dataset["minFitList"]
@@ -56,9 +62,7 @@ def plotGraf():
     axes[1, 1].set_title("Fitness Minimo da versao 2")
     axes[1, 1].set_ylabel("Fitness Minimo")
     axes[1, 1].set_xlabel("Geracao")
-
-
-
+    axes[1, 1].annotate(stringData2, (15, 12))
 
 
     plt.show()
