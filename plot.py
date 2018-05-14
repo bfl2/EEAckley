95a409b2eb555de29db89d98a6e973bf2740930c
bfl2 <brunofilizolaleal@gmail.com>
2018-05-14 15:11:52 -0300

    Funcao de plot com repeticoes adicionada

--- plot.py
+++ plot.py
@@ -13,56 +13,160 @@ def dummyDataset():
 
     return dataset
 
+def absAvg(list):
+    sum=0
+    for e in list:
+        sum+=abs(e)
+    res = sum/len(list)
+    return res
 
-def plotGraf():
+
+def plotGraf():#plota o grafico rodando somente 1 vez o algoritmo
 
     dataset =  ack.EEAckley()
 
     print("AvgFit:{} | MinFit:{}".format(dataset["avgFitList"], dataset["minFit"]))
     #dataset = dummyDataset()
-    fig, axes = plt.subplots(2, 2)
+    fig = plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+    stringData1 = "Fitness Medio: " + str(round(dataset["avgFit"], 5))
+    stringData2 = "Fitness Minimo: " + str(round(dataset["minFit"], 5))
+    stringData = stringData1+"\n"+stringData2
+
+    x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
+    #fitness medio
+    y = dataset["avgFitList"]
+    axes.plot(x,y,label="fitness medio")
+    axes.set_title("Media e minimo de fitness da versao 1")
+    axes.set_ylabel("Fitness")
+    axes.set_xlabel("Geracao")
+    axes.legend(loc=0)
+    axes.text(0.9,0.8,stringData, horizontalalignment='center',verticalalignment='center',transform=axes.transAxes,bbox=dict(facecolor='blue', alpha=0.2))
+
+    ##Fitness minimo
+    x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
+    y = dataset["minFitList"]
+    axes.plot(x, y, label="fitness minimo")
+    axes.legend(loc=0)
+    axes.annotate(stringData2, (0, 0))
+
+
+    plt.show()
+    ###Versao 2###
+    dataset = ack2.EEAckley2()
+
+    fig = plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    print("AvgFit:{} | MinFit:{}".format(dataset["avgFitList"], dataset["minFit"]))
+    stringData1 = "Fitness Medio: " + str(round(dataset["avgFit"], 5))
+    stringData2 = "Fitness Minimo: " + str(round(dataset["minFit"], 5))
+    stringData = stringData1 + "\n" + stringData2
     x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
     y = dataset["avgFitList"]
-    stringData2 = "Fitness Minimo: "+ str(round(dataset["minFit"],5))
-    stringData1 = "Fitness Medio: " + str(round(dataset["avgFit"],5))
-    print(len(x),len(y))
-    axes[0,0].plot(x,y)
-    axes[0, 0].set_title("Media de fitness da versao 1")
-    axes[0, 0].set_ylabel("Fitness Medio")
-    axes[0, 0].set_xlabel("Geracao")
-    axes[0, 0].annotate(stringData1,(15,12))
+    axes.plot(x,y, label="fitness medio")
+    axes.set_title("Media e minimo de fitness da versao 2")
+    axes.set_ylabel("Fitness")
+    axes.set_xlabel("Geracao")
+    axes.legend(loc=0)
+    axes.text(0.9, 0.8, stringData, horizontalalignment='center', verticalalignment='center', transform=axes.transAxes,
+              bbox=dict(facecolor='blue', alpha=0.2))
+
 
     x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
     y = dataset["minFitList"]
-    print(len(x), len(y))
-    axes[0, 1].plot(x, y)
-    axes[0, 1].set_title("Fitness Minimo da versao 1")
-    axes[0, 1].set_ylabel("Fitness Minimo")
-    axes[0, 1].set_xlabel("Geracao")
-    axes[0, 1].annotate(stringData2, (15, 12))
 
-    dataset = ack2.EEAckley2()
+    axes.plot(x, y,label="fitness minimo")
+    axes.legend(loc=0)
 
-    print("AvgFit:{} | MinFit:{}".format(dataset["avgFitList"], dataset["minFit"]))
-    stringData2 = "Fitness Minimo: " + str(round(dataset["minFit"], 5))
-    stringData1 = "Fitness Medio: " + str(round(dataset["avgFit"], 5))
+
+
+    plt.show()
+    return
+
+def plotGrafRep(reps):
+    minFitList = []
+    avgFitList = []
+    xiAbsAvgList = []
+
+    for e in range(0,reps): ## calculando as medias dos fitness minimo e medio finais do algoritmo
+        dataset =  ack.EEAckley()
+        minFitList.append(dataset["minFit"])
+        avgFitList.append(dataset["avgFit"])
+        xiAbsAvgList.append(absAvg(dataset["bestIndiv"]))
+
+    avgFitListMean = sum(avgFitList)/len(avgFitList)
+    avgFitListStd = np.std(avgFitList)
+    minFitListMean = sum(minFitList)/len(minFitList)
+    minFitListStd = np.std(minFitList)
+
+
+    fig = plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+    stringData1 = "Resultados de {} repetiçoes".format(reps)
+    stringData2 = "Fitness Medio:{} || Desvio padrao:{}".format(round(avgFitListMean, 5),round(avgFitListStd, 5))
+    stringData3 = "Fitness Minimo:{} || Desvio padrao:{}".format(round(minFitListMean, 5),round(minFitListStd, 5))
+    stringData4 = "Media dos elementos do cromossomo:{}".format(round(absAvg(dataset["bestIndiv"]), 5))
+    stringData = stringData1 + "\n" + stringData2 + "\n" + stringData3+"\n" + stringData4
+
+    x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
+    #fitness medio
+    y = dataset["avgFitList"]
+    axes.plot(x,y,label="fitness medio")
+    axes.set_title("Media e minimo de fitness da versao 1")
+    axes.set_ylabel("Fitness")
+    axes.set_xlabel("Geracao")
+    axes.legend(loc=0)
+    axes.text(0.8,0.8,stringData, horizontalalignment='center',verticalalignment='center',transform=axes.transAxes,bbox=dict(facecolor='blue', alpha=0.2))
+
+    ##Fitness minimo
+    x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
+    y = dataset["minFitList"]
+    axes.plot(x, y, label="fitness minimo")
+    axes.legend(loc=0)
+    axes.annotate(stringData2, (0, 0))
+
+
+    plt.show()
+    ###Versao 2###
+    minFitList = []
+    avgFitList = []
+    xiAbsAvgList = []
+    for e in range(0, reps):  ## calculando as medias dos fitness minimo e medio finais do algoritmo
+        dataset = ack2.EEAckley2()
+        minFitList.append(dataset["minFit"])
+        avgFitList.append(dataset["avgFit"])
+    avgFitListMean = sum(avgFitList) / len(avgFitList)
+    avgFitListStd = np.std(avgFitList)
+    minFitListMean = sum(minFitList) / len(minFitList)
+    minFitListStd = np.std(minFitList)
+
+    fig = plt.figure()
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+
+    stringData1 = "Resultados de {} repetiçoes".format(reps)
+    stringData2 = "Fitness Medio:{} || Desvio padrao:{}".format(round(avgFitListMean, 5), round(avgFitListStd, 5))
+    stringData3 = "Fitness Minimo:{} || Desvio padrao:{}".format(round(minFitListMean, 5), round(minFitListStd, 5))
+    stringData4 = "Media dos elementos do cromossomo:{}".format(round(absAvg(dataset["bestIndiv"]), 5))
+    stringData = stringData1 + "\n" + stringData2 + "\n" + stringData3 + "\n" + stringData4
     x = np.linspace(0, dataset["generationCount"],dataset["generationCount"])
     y = dataset["avgFitList"]
-    print(len(x),len(y))
-    axes[1,0].plot(x,y)
-    axes[1, 0].set_title("Media de fitness da versao 2")
-    axes[1, 0].set_ylabel("Fitness Medio")
-    axes[1, 0].set_xlabel("Geracao")
-    axes[1, 0].annotate(stringData1, (15, 12))
+    axes.plot(x,y, label="fitness medio")
+    axes.set_title("Media e minimo de fitness da versao 2")
+    axes.set_ylabel("Fitness")
+    axes.set_xlabel("Geracao")
+    axes.legend(loc=0)
+    axes.text(0.9, 0.8, stringData, horizontalalignment='center', verticalalignment='center', transform=axes.transAxes,
+              bbox=dict(facecolor='blue', alpha=0.2))
 
-    #x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
+
+    x = np.linspace(0, len(dataset["minFitList"]), len(dataset["minFitList"]))
     y = dataset["minFitList"]
-    print(len(x), len(y))
-    axes[1, 1].plot(x, y)
-    axes[1, 1].set_title("Fitness Minimo da versao 2")
-    axes[1, 1].set_ylabel("Fitness Minimo")
-    axes[1, 1].set_xlabel("Geracao")
-    axes[1, 1].annotate(stringData2, (15, 12))
+
+    axes.plot(x, y,label="fitness minimo")
+    axes.legend(loc=0)
+
 
 
     plt.show()
@@ -70,4 +174,5 @@ def plotGraf():
 
 
 if __name__ == "__main__":
-    plotGraf()
+    #plotGraf()
+    plotGrafRep(5)
